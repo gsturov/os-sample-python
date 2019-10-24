97093faaf611e8d8e78d8e4b0bf70573d52358f5
gleb.sturov <gsturov@ca.ibm.com>
2019-10-24 11:25:32 -0400

    temp

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,11 @@
-from flask import Flask
-from werkzeug.wsgi import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
-from werkzeug.serving import run_simple
-application = Flask(__name__)
-
-from prometheus_client import Counter
-c = Counter('__test_counter', 'Description of counter')
-c.inc()
+from wsgiref.simple_server import make_server
 
 print('____ adding metric _____')
+from prometheus_client import Counter
+c = Counter('__test_counter', 'Description of counter')
+c.inc(500)
 
-@application.route("/")
-def hello():
-    c.inc()
-    return "Hello World 2!"
-
-print('____ adding metrics handler _____')
-app_dispatch = DispatcherMiddleware(application, {
-    '/metrics': make_wsgi_app()
-})
-run_simple(hostname="localhost", port=8090, application=app_dispatch)
+app = make_wsgi_app()
+httpd = make_server('', 8080, app)
+httpd.serve_forever()
